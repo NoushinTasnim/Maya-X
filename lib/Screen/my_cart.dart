e8e72a8caf2c8f85ce4721f5ab956f68b5821d15
NoushinTasnim <noushintasnim.2615@gmail.com>
2024-05-21 16:59:12 +0600

    my cart updated from order model

--- lib/Screen/my_cart.dart
+++ lib/Screen/my_cart.dart
@@ -27,301 +27,136 @@ class _MyCartScreenState extends State<MyCartScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kPrimaryColor,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: kAccentColor,
+        onPressed: (){
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => CheckOutScreen(),
+            ),
+          );
+        },
+        child: Icon(
+          Icons.shopping_cart_checkout_rounded,
+          color: kPrimaryColor,
+        ),
+      ),
       appBar: AppBar(
         backgroundColor: kAccentColor,
         title: Text(
-            'আমার কার্ট',
-            style: TextStyle(
-              fontFamily: 'Kalpurush'
-            ),
+          'আমার কার্ট',
+          style: TextStyle(
+            fontFamily: 'Kalpurush',
+            color: kPrimaryColor,
+          ),
         ),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  InkWell(
-                    onTap: (){
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => BottomNavScreen(),
-                        ),
-                      );
-                    },
-                    child: Text(
-                      'আরও পণ্য যোগ করুন',
-                      style: TextStyle(
-                        fontFamily: 'Kalpurush',
-                        fontWeight: FontWeight.bold,
-                        color: kAccentColor,
-                      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.only(bottom: 80), // Add padding to avoid content being hidden by the fixed button
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => BottomNavScreen(),
                     ),
-                  ),
-                  SizedBox(
-                    height: 16,
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(16)
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Row(
-                        children: [
-                          Image.asset(
-                            'images/image 2.png',
-                            height: 100,
-                          ),
-                          Expanded(
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  'ফ্রিডম Super Dry স্যানিটারী ন্যাপকিন - ৮টি প্যাড',
-                                  style: TextStyle(
-                                      fontFamily: 'Kalpurush',
-                                      fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                Text(
-                                  '১১০ টাকা',
-                                  style: TextStyle(
-                                      fontFamily: 'Kalpurush'
-                                  ),
-                                )
-                              ],
-                            ),
-                          ),
-                          Column(
-                            children: [
-                              InkWell(
-                                onTap:(){
-                                  setState(() {
-                                    if(_counter<10)
-                                      _counter++;
-                                  });
-                                },
-                                child: Icon(
-                                  Icons.add_circle,
-                                  color: kAccentColor,
-                                ),
-                              ),
-                              Text(
-                                '$_counter'
-                              ),
-                              InkWell(
-                                onTap:(){
-                                  setState(() {
-                                    if(_counter>0)
-                                      _counter--;
-                                  });
-                                },
-                                child: Icon(
-                                  Icons.remove_circle,
-                                  color: kAccentColor,
-                                ),
-                              )
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 16,
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(16)
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Row(
-                        children: [
-                          Image.asset(
-                            'images/image 2.png',
-                            height: 100,
-                          ),
-                          Expanded(
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  'ফ্রিডম Super Dry স্যানিটারী ন্যাপকিন - ৮টি প্যাড',
-                                  style: TextStyle(
-                                      fontFamily: 'Kalpurush',
-                                      fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                Text(
-                                  '১১০ টাকা',
-                                  style: TextStyle(
-                                      fontFamily: 'Kalpurush'
-                                  ),
-                                )
-                              ],
-                            ),
-                          ),
-                          Column(
-                            children: [
-                              Icon(
-                                Icons.add_circle,
-                                color: kAccentColor,
-                              ),
-                              Text(
-                                  '১'
-                              ),
-                              Icon(
-                                Icons.remove_circle,
-                                color: kAccentColor,
-                              )
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      '১x ফ্রিডম Super Dry স্যানিটারী ন্যাপকিন - ৮টি প্যাডঃ ',
-                      style: TextStyle(
-                          fontFamily: 'Kalpurush',
-                          color: kSecondaryColor.withOpacity(.6)
-                      ),
-                    ),
-                    Text(
-                      '১১০ টাকা',
-                      style: TextStyle(
-                          fontFamily: 'Kalpurush',
-                          color: kSecondaryColor.withOpacity(.6)
-                      ),
-                    )
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      '১x ফ্রিডম Super Dry স্যানিটারী ন্যাপকিন - ৮টি প্যাডঃ ',
-                      style: TextStyle(
-                          fontFamily: 'Kalpurush',
-                          color: kSecondaryColor.withOpacity(.6)
-                      ),
-                    ),
-                    Text(
-                      '১১০ টাকা',
-                      style: TextStyle(
-                          fontFamily: 'Kalpurush',
-                          color: kSecondaryColor.withOpacity(.6)
-                      ),
-                    )
-                  ],
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 16.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'ডেলিভারী চার্জঃ ',
-                        style: TextStyle(
-                            fontFamily: 'Kalpurush',
-                            color: kSecondaryColor.withOpacity(.6)
-                        ),
-                      ),
-                      Text(
-                        '৫০ টাকা',
-                        style: TextStyle(
-                            fontFamily: 'Kalpurush',
-                            color: kSecondaryColor.withOpacity(.6)
-                        ),
-                      )
-                    ],
+                  );
+                },
+                child: Text(
+                  'আরও পণ্য যোগ করুন',
+                  textAlign: TextAlign.end,
+                  style: TextStyle(
+                    fontFamily: 'Kalpurush',
+                    fontWeight: FontWeight.bold,
+                    color: kAccentColor,
                   ),
                 ),
-                Divider(
-                  thickness: 1,
-                  color: kSecondaryColor.withOpacity(.2),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 16.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'সর্বমোটঃ ',
-                        style: TextStyle(
-                            fontFamily: 'Kalpurush',
-                            color: kSecondaryColor
-                        ),
-                      ),
-                      Text(
-                        '২৭০ টাকা',
-                        style: TextStyle(
-                            fontFamily: 'Kalpurush',
-                            color: kSecondaryColor
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Container(
-            width: double.infinity,
-            decoration: BoxDecoration(
-              color: kAccentColor,
-              borderRadius: BorderRadius.circular(16)
-            ),
-            padding: EdgeInsets.all(16),
-            child: InkWell(
-              onTap: (){
-                Navigator.push(context,
-                  MaterialPageRoute(
-                    builder: (context) => CheckOutScreen(),
-                  ),
-                );
-              },
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Text(
-                    'চেকআউট করুন',
-                    style: TextStyle(
-                      color: kPrimaryColor,
-                      fontFamily: 'Kalpurush',
-                      fontSize: 16,
-                      fontWeight: FontWeight.w700,
-                    ),
-                  ),
-                  Icon(
-                    Icons.navigate_next,
-                    color: kPrimaryColor,
-                  )
-                ],
               ),
             ),
-          ),
-        ],
+            FutureBuilder<List<Order>>(
+              future: _futureOrders,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  return Center(child: Text('Error: ${snapshot.error}'));
+                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                  return Center(child: Text('No products available'));
+                }
+
+                final orders = snapshot.data!;
+                return Column(
+                  children: orders.map((order) {
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(16),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
+                                children: [
+                                  Image.asset(
+                                    order.image,
+                                    height: 100,
+                                  ),
+                                  Expanded(
+                                    child: Column(
+                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          order.name,
+                                          style: TextStyle(
+                                            fontFamily: 'Kalpurush',
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                        Text(
+                                          order.amount,
+                                          style: TextStyle(
+                                            fontFamily: 'Kalpurush',
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  Column(
+                                    children: [
+                                      Icon(
+                                        Icons.add_circle,
+                                        color: kAccentColor,
+                                      ),
+                                      Text("${order.quantity}"),
+                                      Icon(
+                                        Icons.remove_circle,
+                                        color: kAccentColor,
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  }).toList(),
+                );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
